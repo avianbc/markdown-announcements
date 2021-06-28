0927a5366e8a47ad824f0d451d8286998e521b2f
Brad <bcarey@getitc.com>
2021-06-27 23:23:49 -0400

    Add dropdown to toggle # of items per page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MarkdownModule } from 'ngx-markdown';
@@ -22,6 +23,7 @@ import { AppComponent } from './app.component';
     MarkdownModule.forRoot(),
     MatButtonModule,
     MatExpansionModule,
+    MatSelectModule,
     NgxPaginationModule,
     HttpClientModule,
   ],
